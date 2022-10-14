a9f2fdd200487f59a78808ee2a0c506d8f80ac2e
Mehdi-Fathollah <mehdi.fathollah.222@gmail.com>
2022-10-15 00:03:56 +0330

    Updating and finished header brands

--- scripts/main.js
+++ scripts/main.js
@@ -1,3 +1,4 @@
+// Hamburger Menu
 const navHam = document.querySelector(".nav-ham-menu");
 const mobileNav = document.querySelector(".mobile-nav");
 
@@ -14,3 +15,42 @@ navHam.addEventListener("click", function () {
     navOpen = true;
   }
 });
+
+// Header Brands Slider
+const container = document.querySelector(".header-brands");
+const cards = document.querySelector(".header-brands__img-container");
+
+let isPressedDown = false;
+let cursorXSpace;
+
+container.addEventListener("mousedown", (e) => {
+  isPressedDown = true;
+  cursorXSpace = e.offsetX - cards.offsetLeft;
+  container.style.cursor = "grabbing";
+});
+
+container.addEventListener("mouseup", () => {
+  container.style.cursor = "grab";
+});
+
+window.addEventListener("mouseup", () => {
+  isPressedDown = false;
+});
+
+container.addEventListener("mousemove", (e) => {
+  if (!isPressedDown) return;
+  e.preventDefault();
+  cards.style.left = `${e.offsetX - cursorXSpace}px`;
+  boundCards();
+});
+
+function boundCards() {
+  const container_rect = container.getBoundingClientRect();
+  const cards_rect = cards.getBoundingClientRect();
+
+  if (parseInt(cards.style.left) > 0) {
+    cards.style.left = 0;
+  } else if (cards_rect.right < container_rect.right) {
+    cards.style.left = `-${cards_rect.width - container_rect.width}px`;
+  }
+}

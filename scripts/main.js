6553bb1b05347ab5f585a84a95a9894aa2c68bf7
Mehdi-Fathollah <mehdi.fathollah.222@gmail.com>
2022-10-15 02:40:43 +0330

    Rss validation and alert added

--- scripts/main.js
+++ scripts/main.js
@@ -143,3 +143,75 @@ function removeOpen(index1) {
     }
   });
 }
+
+// RSS Validation And Alert
+let email = document.querySelector(".rss-news-form__email-input");
+let success = document.querySelector(".successful-alert-overlay");
+let formButton = document.querySelector(".rss-news-form__submitation-button");
+let validEmail = document.querySelector(".rss-news-form-valid-alert-text");
+let inValidEmail = document.querySelector(".rss-news-form-failed-alert-text");
+let danger = document.querySelector(".rss-news-form-empty-field-alert-text");
+let successClose = document.querySelector(".successful-alert__button");
+let form = document.querySelector(".rss-news-form__form");
+let pattern = /^[^ ]+@[^ ]+\.[a-z]{2,3}$/;
+
+formButton.onclick = function () {
+  if (email.value.length === 0) {
+    danger.classList.add("rss-news-form-empty-field-alert-text--show");
+    validEmail.classList.remove("rss-news-form-valid-alert-text--show");
+    inValidEmail.classList.remove("rss-news-form-failed-alert-text--show");
+  } else {
+    success.classList.add("successful-alert-overlay--show");
+    document.body.style.overflowY = "hidden";
+  }
+};
+
+form.addEventListener("input", () => {
+  if (email.value.length === 0) {
+    formButton.disabled = false;
+  }
+});
+
+email.oninput = function () {
+  if (email.value.match(pattern)) {
+    validEmail.classList.add("rss-news-form-valid-alert-text--show");
+    inValidEmail.classList.remove("rss-news-form-failed-alert-text--show");
+    danger.classList.remove("rss-news-form-empty-field-alert-text--show");
+    formButton.disabled = false;
+  } else {
+    inValidEmail.classList.add("rss-news-form-failed-alert-text--show");
+    validEmail.classList.remove("rss-news-form-valid-alert-text--show");
+    danger.classList.remove("rss-news-form-empty-field-alert-text--show");
+    formButton.disabled = true;
+  }
+};
+
+// Functional Buttons
+successClose.onclick = function () {
+  success.classList.remove("successful-alert-overlay--show");
+  danger.classList.remove("rss-news-form-empty-field-alert-text--show");
+  validEmail.classList.remove("rss-news-form-valid-alert-text--show");
+  inValidEmail.classList.remove("rss-news-form-failed-alert-text--show");
+  formButton.classList.add("rss-news-form__submitation-button--disabled");
+  document.body.style.overflowY = "auto";
+  formButton.disabled = true;
+  email.disabled = true;
+  email.value = "";
+};
+
+success.onclick = function () {
+  success.classList.remove("successful-alert-overlay--show");
+  danger.classList.remove("rss-news-form-empty-field-alert-text--show");
+  validEmail.classList.remove("rss-news-form-valid-alert-text--show");
+  inValidEmail.classList.remove("rss-news-form-failed-alert-text--show");
+  formButton.classList.add("rss-news-form__submitation-button--disabled");
+  document.body.style.overflowY = "auto";
+  formButton.disabled = true;
+  email.disabled = true;
+  email.value = "";
+};
+
+//* Preventing 'form' from refreshing the page
+function refresher() {
+  event.preventDefault();
+}
